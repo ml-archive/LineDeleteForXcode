187a2a76f8162a9aaf8afd9db982d26727af19c6
Dominik Hadl <nick@dynamicdust.com>
2016-09-17 08:58:30 +0200

    Implement line delete command

---
+++ Extension/DeleteLineCommand.swift
@@ -0,0 +1,45 @@
+//
+//  DeleteLineCommand.swift
+//  Extension
+//
+//  Created by Dominik Hádl on 17/09/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class DeleteLineCommand: NSObject, XCSourceEditorCommand {
+
+    func perform(with invocation: XCSourceEditorCommandInvocation,
+                 completionHandler: @escaping (Error?) -> Void ) -> Void {
+
+        // Cancel early
+        invocation.cancellationHandler = {
+            completionHandler(nil)
+        }
+
+        // Delete the line
+        do {
+            try deleteLinesFromCurrentBuffer(buffer: invocation.buffer)
+        } catch {
+            completionHandler(error)
+        }
+
+        // Finish
+        completionHandler(nil)
+    }
+
+    func deleteLinesFromCurrentBuffer(buffer: XCSourceTextBuffer) throws {
+        guard let selection = buffer.selections.firstObject as? XCSourceTextRange,
+            selection.start.line > 0, selection.end.line < buffer.lines.count else {
+                throw NSError(domain: "com.nodes.DeleteLineForXcode",
+                              code: 1,
+                            userInfo: [NSLocalizedDescriptionKey : "Error while getting current line."])
+        }
+
+        let range = NSRange(location: selection.start.line,
+                            length: selection.end.line - selection.start.line)
+        buffer.lines.remove(range)
+    }
+}
---
+++ Extension/DeleteLineExtension.swift
@@ -0,0 +1,27 @@
+//
+//  DeleteLineExtension.swift
+//  Extension
+//
+//  Created by Dominik Hádl on 17/09/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class DeleteLineExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}

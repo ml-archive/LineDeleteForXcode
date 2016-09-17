3f6824c4c9135ccaeb5a62625fe533b08e2c7c7e
Dominik Hadl <nick@dynamicdust.com>
2016-09-17 09:26:25 +0200

    various bugfixes and workarounds for Xcode

--- Extension/DeleteLineCommand.swift
+++ Extension/DeleteLineCommand.swift
@@ -23,6 +23,7 @@ class DeleteLineCommand: NSObject, XCSourceEditorCommand {
         do {
             try deleteLinesFromCurrentBuffer(buffer: invocation.buffer)
         } catch {
+            // Return with error if we fail to delete
             completionHandler(error)
         }
 
@@ -32,18 +33,31 @@ class DeleteLineCommand: NSObject, XCSourceEditorCommand {
 
     func deleteLinesFromCurrentBuffer(buffer: XCSourceTextBuffer) throws {
         guard let selection = buffer.selections.firstObject as? XCSourceTextRange,
-            selection.start.line > 0, selection.end.line < buffer.lines.count else {
+            selection.start.line >= 0, selection.end.line <= buffer.lines.count else {
                 throw NSError(domain: "com.nodes.DeleteLineForXcode",
                               code: 1,
                             userInfo: [NSLocalizedDescriptionKey : "Error while getting current line."])
         }
 
         if selection.start.line == selection.end.line {
-            buffer.lines.removeObject(at: selection.start.line)
+            // Delete one line
+            if selection.end.line == buffer.lines.count {
+                buffer.lines.removeLastObject()
+            } else {
+                buffer.lines.removeObject(at: selection.start.line)
+            }
         } else {
+            // Delete range of lines
             let range = NSRange(location: selection.start.line,
                                 length: selection.end.line - selection.start.line)
             buffer.lines.removeObjects(in: range)
+        }
+
+        // Make sure there is a selection, fail safe for Xcode
+        if buffer.selections.count == 0 {
+            let position  = XCSourceTextPosition(line: 0, column: 0)
+            let selection = XCSourceTextRange(start: position, end: position)
+            buffer.selections.add(selection)
         }
     }
 }
